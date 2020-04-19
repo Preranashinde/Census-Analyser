f86e9e2757720a6460cecbd03ae3c148ecf19206
Preranashinde <preranapshinde3009@gmail.com>
2020-04-18 22:44:09 -0300

    "ADDED:Code to check csv file records correct but delimiter incorrect returns a custom exception"

--- src/main/java/censusanalyser/CensusAnalyser.java
+++ src/main/java/censusanalyser/CensusAnalyser.java
@@ -32,6 +32,9 @@ public class CensusAnalyser {
         } catch (IOException e) {
             throw new CensusAnalyserException(e.getMessage(),
                     CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM);
+        } catch (RuntimeException e) {
+            throw new CensusAnalyserException(e.getMessage(),
+                    CensusAnalyserException.ExceptionType.WRONG_FILE_DELIMITER);
         }
     }
 }
